9e2759441bdc35cc0d762d30c0195f088c58523c
Matjaž Horvat <matjaz.horvat@gmail.com>
2013-02-02 23:23:44 +0100

    Document JS Code

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
 $(function() {
+
+  /* L20n */
+
+  var parser = new L20nParser(L20nEventEmitter);
+  var compiler = new L20nCompiler(L20nEventEmitter, L20nParser);
+
+  parser.addEventListener('error', function(e) {
+    $("#errors").append("<dt>Syntax error at position " + e.pos + "</dt><dd>" + e.message + "</dd>");
+  });
+  compiler.addEventListener('error', function(e) {
+    $("#errors").append("<dt>" + e.name + " in entity <code>" + e.entry + "</code></dt><dd>" + e.message + "</dd>");
+  });
+
+  var source = ace.edit("source");
+  source.getSession().setMode("ace/mode/clojure");
+
+
+
+  /* ACE */
+
+  source.getSession().on('change', function(e) {
+    $("#output").empty();
+    $("#errors").empty();
+    var code = source.getValue();
+    var ast = parser.parse(code);
+    var entries = compiler.compile(ast);
+    for (var id in entries) {
+      if (entries[id].expression) {
+        $("#output").append("<div><dt><code>" + id + "()</code></dt><dd><em>macro</em></dd></div>");
+        continue;
+      }
+      var val;
+      try {
+        val = entries[id].toString();
+      } catch (e) {
+        if (e instanceof compiler.ValueError) {
+          val = e.source;
+        } else {
+          continue;
+        }
+      }
+      $("#output").append("<div><dt><code>" + id + "</code></dt><dd>" + val + "</dd></div>");
+    }
+  });
+
+
+
+  /* Linkify */
+
+  function utf8_to_b64(str) {
+      return window.btoa(unescape(encodeURIComponent(str)));
+  }
+   
+  function b64_to_utf8(str) {
+      return decodeURIComponent(escape(window.atob(str)));
+  }
+
+  function linkify() {
+    var code = source.getValue();
+    console.log(utf8_to_b64(code));
+  }
+
+
+
+  /* Popover */
+
   $('#element').popover({
     placement: 'bottom',
     html: true,
@@ -7,66 +73,20 @@ $(function() {
   }).click(function() {
     $(this).popover('toggle');
   });
-});
 
-var source = ace.edit("source");
-source.getSession().setMode("ace/mode/clojure");
 
-var parser = new L20nParser(L20nEventEmitter);
-var compiler = new L20nCompiler(L20nEventEmitter, L20nParser);
 
-parser.addEventListener('error', function(e) {
-  $("#errors").append("<dt>Syntax error at position " + e.pos + "</dt><dd>" + e.message + "</dd>");
-});
-compiler.addEventListener('error', function(e) {
-  $("#errors").append("<dt>" + e.name + " in entity <code>" + e.entry + "</code></dt><dd>" + e.message + "</dd>");
-});
+  /* Main Code */
 
-source.getSession().on('change', function(e) {
-  $("#output").empty();
-  $("#errors").empty();
-  var code = source.getValue();
-  var ast = parser.parse(code);
-  var entries = compiler.compile(ast);
-  for (var id in entries) {
-    if (entries[id].expression) {
-      $("#output").append("<div><dt><code>" + id + "()</code></dt><dd><em>macro</em></dd></div>");
-      continue;
-    }
-    var val;
-    try {
-      val = entries[id].toString();
-    } catch (e) {
-      if (e instanceof compiler.ValueError) {
-        val = e.source;
-      } else {
-        continue;
-      }
-    }
-    $("#output").append("<div><dt><code>" + id + "</code></dt><dd>" + val + "</dd></div>");
+  var hash = window.location.hash.slice(1);
+  var content = (
+    "<name \"L20n tinker\">\n" +
+    "<welcome \"Welcome to {{ name }}\">\n"
+  );
+
+  if (hash) {
+    content = b64_to_utf8(hash);
   }
+  source.setValue(content);
+
 });
-
-function utf8_to_b64(str) {
-    return window.btoa(unescape(encodeURIComponent(str)));
-}
- 
-function b64_to_utf8(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-}
-
-var hash = window.location.hash.slice(1);
-var content = (
-  "<name \"L20n tinker\">\n" +
-  "<welcome \"Welcome to {{ name }}\">\n"
-);
-
-if (hash) {
-  content = b64_to_utf8(hash);
-}
-source.setValue(content);
-
-function linkify() {
-  var code = source.getValue();
-  console.log(utf8_to_b64(code));
-}
