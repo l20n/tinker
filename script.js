3e5cf58532f5d37421965277e350acd63817c5ac
Matjaž Horvat <matjaz.horvat@gmail.com>
2013-02-02 21:07:49 +0100

    Remove headers

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $(function() {
-	$('.span4').hover(function() {
-		$(this).find('header').fadeOut('slow');
-	}, function() {
-		$(this).find('header').fadeIn('slow');
-	});
+
 });
 
 var source = ace.edit("source");
