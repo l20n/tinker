3eabde780d83f031d7ef97412cfd2afd7d59028d
Staś Małolepszy <stas@mozilla.com>
2016-04-29 14:01:36 +0200

    code drop: FTL tinker

--- script.js
+++ script.js
@@ -1,55 +1,102 @@
 $(function() {
 
+  var config = {
+    escapeHtml: true,
+    lang: 'en-US',
+  };
+
   /* L20n */
-
-  var parser = new L20n.Parser();
-  var compiler = new L20n.Compiler();
-
-  var _retr = new L20n.RetranslationManager();
-  compiler.setGlobals(_retr.globals);
-
-  var entries;
-  var data;
 
   function update() {
     $("#errors").empty();
     $("#output").empty();
 
-    var code = source.getValue();
-    var ast = parser.parse(code);
-    try {
-      data = JSON.parse(context.getValue());
-    } catch (e) {}
-    entries = compiler.compile(ast);
+    var args;
 
-    for (var id in entries) {
-      if (entries[id].expression) {
+    try {
+      args = JSON.parse(context.getValue());
+    } catch (e) {}
+
+    var code = source.getValue();
+    try {
+      var [resource, errors] = L20n.FTLASTParser.parseResource(code);
+      var [entries] = L20n.createEntriesFromAST([resource, errors]);
+    } catch(e) {
+      logUnexpected(e);
+    }
+
+    errors.forEach(e => {
+      $("#errors").append(
+        "<dt>" + e.name + " in line " + e._pos.row + ", column " + e._pos.col +"</dt>" +
+        "<dd>" + escapeHtml(e.description) + "</dd>" +
+        "<dd><pre>" + prettyContext(e.context, e.offset) + "</pre></dd>"
+      );
+    });
+
+    const anots = errors.map(e => ({
+      type: 'error',
+      text: e.message,
+      row: e._pos.row - 1,
+      column: e._pos.col,
+    }));
+
+    source.getSession().setAnnotations(anots);
+
+    print(resource.body, new L20n.MockContext(entries), args, entries);
+  }
+
+  function print(body, ctx, args, entries) {
+    for (let entry of body) {
+      if (entry.type === 'Comment') {
         continue;
-        $("#output").append("<div><dt><code class=\"disabled\">" + id + "()</code></dt><dd></dd></div>");
       }
-      var val;
-      try {
-        val = entries[id].getString(data);
-      } catch (e) {
-        if (e.source) {
-          val = e.source;
-        } else {
-          $("#output").append("<div><dt><code class=\"disabled\">" + e.entry + "</code></dt><dd></dd></div>");
-          continue;
+
+      if (entry.type === 'JunkEntry') {
+        $("#output").append(
+          "<div><dt><code class='disabled'>JunkEntry</code></dt>" +
+          "<dd><pre>" + escapeHtml(entry.content, true) + "</pre></dd></div>"
+        );
+        continue;
+      }
+
+      if (entry.type === 'Section') {
+        print(entry.body, ctx, args, entries);
+        continue;
+      }
+
+      if (entry.type === 'Entity') {
+        const lang = { code: config.lang };
+        const id = entry.id.name;
+
+        try {
+          const result = L20n.format(ctx, lang, args, entries[id]);
+          $("#output").append(
+            "<div><dt><code>" + id + "</code></dt>" +
+            "<dd>" + escapeHtml(result[1]) + "</dd></div>"
+          );
+          result[0].forEach(e => {
+            $("#errors").append(
+              "<dt>" + e.name + " in entity <code>" + id + "</code></dt>" +
+              "<dd>" + escapeHtml(e.message) + "</dd>"
+            );
+          });
+        } catch(e) {
+          logUnexpected(e);
         }
       }
-      $("#output").append("<div><dt><code>" + id + "</code></dt><dd>" + val + "</dd></div>");
     }
   }
 
   /* ACE */
 
   var source = ace.edit("source");
+  source.setBehavioursEnabled(false);
   source.setShowPrintMargin(false);
   source.setDisplayIndentGuides(false);
   source.getSession().setUseWrapMode(true);
+  source.getSession().setOption("useWorker", false);
   source.setTheme("ace/theme/solarized_light");
-  source.getSession().setMode("ace/mode/php");
+  source.getSession().setMode("ace/mode/ft");
   source.getSession().on('change', update);
 
   var context = ace.edit("context");
@@ -63,17 +110,42 @@ $(function() {
 
   /* Errors */
 
-  parser.addEventListener('error', function(e) {
-    var pos = source.getSession().getDocument().indexToPosition(e.pos);
-    $("#errors").append("<dt>Syntax error near row " + (pos.row + 1) + 
-                        ", column " + (pos.column + 1) + "</dt><dd>" + 
-                        e.message + "</dd>");
-  });
-  compiler.addEventListener('error', function(e) {
-    $("#errors").append("<dt>" + e.name + " in entity <code>" + e.entry + 
-                        "</code></dt><dd>" + e.message + "</dd>");
-  });
+  function logUnexpected(e) {
+    $("#errors").append(
+      "<dt>" + e.name + "</dt><dd>" + escapeHtml(e.message) + "</dd>"
+    );
+  }
 
+  var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  function replaceHtml(char) {
+    return entityMap[char];
+  }
+
+  function escapeHtml(str, force) {
+    return str && (config.escapeHtml || force) ?
+      str.replace(/[&<>"'\/]/g, replaceHtml) : str;
+  }
+
+  function prettyContext(content, offset) {
+    const [ before, char, after ] = [
+      content.substring(0, offset),
+      content.substring(offset, offset + 1),
+      content.substring(offset + 1)
+    ].map(part => escapeHtml(part, true));
+
+    const hilite = char === '\n' ?
+      ' \n' : char;
+
+    return `${before}<em>${hilite}</em>${after}`;
+  }
 
 
   /* Linkify */
@@ -90,19 +162,22 @@ $(function() {
     var state = {
       source: source.getValue(),
       context: context.getValue(),
-    }
-    return window.location.href.split("#")[0] + '#' + utf8_to_b64(JSON.stringify(state));
+      config: config
+    };
+    return window.location.href.split("#")[0] + '#' +
+      utf8_to_b64(JSON.stringify(state));
   }
 
 
-
   /* Main Code */
-
 
   var hash = window.location.hash.slice(1) || defaultHash;
   var state = JSON.parse(b64_to_utf8(hash));
   context.setValue(state.context);
   source.setValue(state.source);
+  if (state.config) {
+    config = state.config;
+  }
   source.clearSelection();
   source.gotoLine(0);
   context.clearSelection();
@@ -117,5 +192,13 @@ $(function() {
     $(this).popover('toggle');
   });
 
-  window.addEventListener("resize", update);
+  $('#escape-html').click(function() {
+    config.escapeHtml = !config.escapeHtml;
+    update();
+  });
+
+  $('#lang').change(function(evt) {
+    config.lang = $(this).val();
+    update();
+  });
 });
